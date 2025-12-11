ce41f42961907f4e3ffe31e13e4657a5680b31a7
Frinky <cuddlysharkyt@gmail.com>
2025-12-11 16:03:53 +1030

    New post about LuaRuntime plugin.

--- content.js
+++ content.js
@@ -106,7 +106,30 @@ window.CONTENT = {
     },
   ],
   posts: [
-      {
+    { // Lua Runtime plugin release
+      date: "11 Dec 2025",
+      title: "LuaRuntime Plugin (UE 5.7)",
+      slug: "luaruntime-release",
+      href: "#",
+      content: text`
+      Just made "my" LuaRuntime plugin for Unreal Engine 5.7 public. It's something I wanted to integrate into a game for a while, but I never had the technical know-how to do it.
+
+      The actual primary goal of this plugin was to test the viability of AI agents for plugin development. 
+
+      As someone who's slightly more focused on the "game" side of programming, I would often find myself with cool ideas for games that I couldn't make because I didn't have the technical skills to implement them. And oftentimes the marketplace plugins either didn't exist or were too expensive, out of date, not supported, or just didn't fit my needs.
+
+      But with the rise of "vibe coding" platforms (terrible name) like Codex and Claude Code, I thought they could perhaps allow me to actually shift my focus away from the technical hurdles and more towards the creative stuff I like doing.
+
+      One of these ideas was to make a game that involved scripting (Replicube, Bitburner, etc.). I'd always heard how embeddable the Lua runtime was, so it seemed like a good fit.
+
+      This plugin is NOT battle-tested or really production-ready, but it does work surprisingly well. The AI tools essentially one-shotted it. I did go and do some further iteration on the BP integration.
+
+      So if you're interested in adding Lua scripting to your Unreal Engine 5 project, check it out! Technically it's a 5.7 plugin, but it should compile pretty easily for any 5.x version (with some minor tweaks).
+
+      Here's the link: https://github.com/frinky04/LuaRuntimePlugin-UnrealEngine
+      `,
+    },
+    { // Transience save wipe guide
       date: "09 Dec 2025",
       title: "How to wipe your Transience save",
       slug: "transience-wipe-save",
@@ -139,16 +162,18 @@ window.CONTENT = {
 
       `,
     },
-    {
+    { // New website post
       date: "07 Dec 2025",
       title: "New website",
       slug: "website-release",
       href: "#",
       content: text`
         Decided to make a new website.
+
+        https://github.com/frinky04/FrinkyWebsiteV2
       `,
     },
-    {
+    { // Birth post
       date: "24 Sep 2004",
       title: "I was born",
       slug: "birth",
